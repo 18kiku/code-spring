766da4475f9df3709622856c2a808d62a23757e6
18kiku <18kiku@gmail.com>
2022-09-23 15:02:25 +0900

    File upload 게시판 글 등록 시 파일 첨부 기능 게시판 글 조회 시 파일 다운로드 기능 게시판 글 수정 시 업로드 된 파일 삭제 기능

--- ex05/src/main/java/org/zerock/controller/UploadController.java
+++ ex05/src/main/java/org/zerock/controller/UploadController.java
@@ -2,18 +2,33 @@ package org.zerock.controller;
 
 import lombok.extern.log4j.Log4j;
 import net.coobird.thumbnailator.Thumbnailator;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+import org.zerock.domain.AttachFileDTO;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.nio.file.Files;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 @Controller
@@ -33,9 +48,9 @@ public class UploadController {
 
         for (MultipartFile multipartFile : uploadFile) {
 
-            log.info("----------------------");
-            log.info("upload file name: " + multipartFile.getOriginalFilename());
-            log.info("upload file size: " + multipartFile.getSize());
+            log.info("-------------------------------------");
+            log.info("Upload File Name: " + multipartFile.getOriginalFilename());
+            log.info("Upload File Size: " + multipartFile.getSize());
 
             File saveFile = new File(uploadFolder, multipartFile.getOriginalFilename());
 
@@ -43,60 +58,15 @@ public class UploadController {
                 multipartFile.transferTo(saveFile);
             } catch (Exception e) {
                 log.error(e.getMessage());
-            }
-        }
+            } // end catch
+        } // end for
+
     }
 
     @GetMapping("/uploadAjax")
     public void uploadAjax() {
-        log.info("upload Ajax");
-    }
 
-    @PostMapping("/uploadAjaxAction")
-    public void uploadAjaxPost(MultipartFile[] uploadFile) {
-
-        log.info("update ajax post......");
-
-        String uploadFolder = "/Users/hwangjeonghwan/upload";
-
-        // Make folder ----
-        File uploadPath = new File(uploadFolder, getFolder());
-        log.info("upload path: " + uploadPath);
-
-        if(uploadPath.exists() == false) {
-            uploadPath.mkdirs();
-        }
-        // Make yyyy/MM/dd folder
-
-        for(MultipartFile multipartFile : uploadFile) {
-            log.info("----------------");
-            log.info("upload file name: " + multipartFile.getOriginalFilename());
-            log.info("upload file size: " + multipartFile.getSize());
-
-            String uploadFileName = multipartFile.getOriginalFilename();
-
-            uploadFileName = uploadFileName.substring(uploadFileName.lastIndexOf("\\") + 1);
-            log.info("only file name: " + uploadFileName);
-
-            UUID uuid = UUID.randomUUID();
-
-            uploadFileName = uuid.toString() + "_" + uploadFileName;
-
-            try {
-                //File saveFile = new File(uploadFolder, uploadFileName);
-                File saveFile = new File(uploadPath, uploadFileName);
-                multipartFile.transferTo(saveFile);
-                // check image type file
-                if(checkImageType(saveFile)) {
-                    FileOutputStream thumbnail = new FileOutputStream(new File(uploadPath, "s_" + uploadFileName));
-
-                    Thumbnailator.createThumbnail(multipartFile.getInputStream(), thumbnail, 100, 100);
-                    thumbnail.close();
-                }
-            } catch (Exception e) {
-                log.error(e.getMessage());
-            }
-        }
+        log.info("upload ajax");
     }
 
     private String getFolder() {
@@ -110,16 +80,181 @@ public class UploadController {
         return str.replace("-", File.separator);
     }
 
+
     private boolean checkImageType(File file) {
 
         try {
             String contentType = Files.probeContentType(file.toPath());
 
             return contentType.startsWith("image");
+
         } catch (IOException e) {
+            // TODO Auto-generated catch block
             e.printStackTrace();
         }
+
         return false;
     }
+
+    @PostMapping(value = "/uploadAjaxAction", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public ResponseEntity<List<AttachFileDTO>> uploadAjaxPost(MultipartFile[] uploadFile) {
+
+        List<AttachFileDTO> list = new ArrayList<>();
+        String uploadFolder = "/Users/hwangjeonghwan/upload";
+
+        String uploadFolderPath = getFolder();
+        // make folder --------
+        File uploadPath = new File(uploadFolder, uploadFolderPath);
+
+        if (uploadPath.exists() == false) {
+            uploadPath.mkdirs();
+        }
+        // make yyyy/MM/dd folder
+
+        for (MultipartFile multipartFile : uploadFile) {
+
+            AttachFileDTO attachDTO = new AttachFileDTO();
+
+            String uploadFileName = multipartFile.getOriginalFilename();
+
+            // IE has file path
+            uploadFileName = uploadFileName.substring(uploadFileName.lastIndexOf("\\") + 1);
+            log.info("only file name: " + uploadFileName);
+            attachDTO.setFileName(uploadFileName);
+
+            UUID uuid = UUID.randomUUID();
+
+            uploadFileName = uuid.toString() + "_" + uploadFileName;
+
+            try {
+                File saveFile = new File(uploadPath, uploadFileName);
+                multipartFile.transferTo(saveFile);
+
+                attachDTO.setUuid(uuid.toString());
+                attachDTO.setUploadPath(uploadFolderPath);
+
+                // check image type file
+                if (checkImageType(saveFile)) {
+
+                    attachDTO.setImage(true);
+
+                    FileOutputStream thumbnail = new FileOutputStream(new File(uploadPath, "s_" + uploadFileName));
+
+                    Thumbnailator.createThumbnail(multipartFile.getInputStream(), thumbnail, 100, 100);
+
+                    thumbnail.close();
+                }
+
+                // add to List
+                list.add(attachDTO);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        } // end for
+        return new ResponseEntity<>(list, HttpStatus.OK);
+    }
+
+    @GetMapping("/display")
+    @ResponseBody
+    public ResponseEntity<byte[]> getFile(String fileName) {
+
+        log.info("fileName: " + fileName);
+
+        File file = new File("/Users/hwangjeonghwan/upload/" + fileName);
+
+        log.info("file: " + file);
+
+        ResponseEntity<byte[]> result = null;
+
+        try {
+            HttpHeaders header = new HttpHeaders();
+
+            header.add("Content-Type", Files.probeContentType(file.toPath()));
+            result = new ResponseEntity<>(FileCopyUtils.copyToByteArray(file), header, HttpStatus.OK);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    @GetMapping(value = "/download", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    @ResponseBody
+    public ResponseEntity<Resource> downloadFile(@RequestHeader("User-Agent") String userAgent, String fileName) {
+
+        Resource resource = new FileSystemResource("/Users/hwangjeonghwan/upload/" + fileName);
+
+        if (resource.exists() == false) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        String resourceName = resource.getFilename();
+
+        // remove UUID
+        String resourceOriginalName = resourceName.substring(resourceName.indexOf("_") + 1);
+
+        HttpHeaders headers = new HttpHeaders();
+        try {
+
+            String downloadName = null;
+
+            if ( userAgent.contains("Trident")) {
+                log.info("IE browser");
+                downloadName = URLEncoder.encode(resourceOriginalName, "UTF-8").replaceAll("\\+", " ");
+            }else if(userAgent.contains("Edge")) {
+                log.info("Edge browser");
+                downloadName =  URLEncoder.encode(resourceOriginalName,"UTF-8");
+            }else {
+                log.info("Chrome browser");
+                downloadName = new String(resourceOriginalName.getBytes("UTF-8"), "ISO-8859-1");
+            }
+
+            log.info("downloadName: " + downloadName);
+
+            headers.add("Content-Disposition", "attachment; filename=" + downloadName);
+
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        return new ResponseEntity<Resource>(resource, headers, HttpStatus.OK);
+    }
+
+    @PostMapping("/deleteFile")
+    @ResponseBody
+    public ResponseEntity<String> deleteFile(String fileName, String type) {
+
+        log.info("deleteFile: " + fileName);
+
+        File file;
+
+        try {
+            file = new File("/Users/hwangjeonghwan/upload/" + URLDecoder.decode(fileName, "UTF-8"));
+
+            file.delete();
+
+            if (type.equals("image")) {
+
+                String largeFileName = file.getAbsolutePath().replace("s_", "");
+
+                log.info("largeFileName: " + largeFileName);
+
+                file = new File(largeFileName);
+
+                file.delete();
+            }
+
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<String>("deleted", HttpStatus.OK);
+
+    }
+
 
 }
